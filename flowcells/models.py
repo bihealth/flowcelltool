39f470e64da3a94ad69d550f098ba07383bafe78
Manuel Holtgrewe <zyklenfrei@gmail.com>
2016-10-02 12:54:31 +0200

    Adding BarcodeSet and BarcodeSetEntry

--- flowcells/models.py
+++ flowcells/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 from markdown_deux.templatetags.markdown_deux_tags import markdown_allowed
 
+# SequencingMachine and related ----------------------------------------------
 
 #: Choices for machine models
 MACHINE_MODELS = (
@@ -29,6 +30,11 @@ INDEX_WORKFLOWS = (
 class SequencingMachine(models.Model):
     """Represent a sequencing machine instance
     """
+
+    #: Timestamp for creation time
+    created_at = models.DateTimeField(auto_now_add=True)
+    #: Timestamp for last update
+    updated_at = models.DateTimeField(auto_now=True)
 
     #: Vendor ID of the machine, reflected in file names and read names later
     #: on
@@ -73,3 +79,76 @@ class SequencingMachine(models.Model):
 
     def __repr__(self):
         return str(self)
+
+
+# BarcodeSet and related -----------------------------------------------------
+
+
+class BarcodeSet(models.Model):
+    """A set of barcodes with id => sequence mapping"""
+
+    #: Timestamp for creation time
+    created_at = models.DateTimeField(auto_now_add=True)
+    #: Timestamp for last update
+    updated_at = models.DateTimeField(auto_now=True)
+
+    #: Full name of the index set
+    name = models.CharField(
+        max_length=100,
+        help_text='Full name of the barcode adapter set')
+
+    #: Short, unique identifier of the barcode index set
+    short_name = models.CharField(
+        max_length=100, unique=True, db_index=True,
+        help_text='Short, unique identifier of barcode adapter set')
+
+    def __str__(self):
+        return '{} ({})'.format(self.name, self.short_name)
+
+    def __repr__(self):
+        tpl = 'BarcodeSet({})'
+        values = (self.name, self.short_name)
+        return tpl.format(', '.join(map(repr, values)))
+
+
+class BarcodeSetEntry(models.Model):
+    """A barcode sequence with an id"""
+
+    #: Timestamp for creation time
+    created_at = models.DateTimeField(auto_now_add=True)
+    #: Timestamp for last update
+    updated_at = models.DateTimeField(auto_now=True)
+
+    #: The barcode set that this barcode belongs to
+    barcode_set = models.ForeignKey(BarcodeSet, related_name='barcodes',
+                                    on_delete=models.CASCADE)
+
+    #: The identifier of the adapter, e.g., 'AR001'.  This has to be unique
+    #: in the context of the ``BarcodeSet``
+    name = models.CharField(max_length=100, db_index=True, unique=False)
+
+    #: DNA sequence of the barcode.  In the case of dual indexing, use the
+    #: sequence as for workflow A
+    sequence = models.CharField(max_length=200)
+
+    def save(self, *args, **kwargs):
+        """Version of save() that ensure uniqueness of the name within the
+        BarcodeSet
+        """
+        self._validate_unique()
+        super().save(*args, **kwargs)
+
+    def _validate_unique(self):
+        """Validates that the name is unique within the BarcodeSet"""
+        qs = BarcodeSetEntry.objects.filter(name=self.name)
+        if qs.filter(barcode_set=self.barcode_set).exists():
+            raise ValidationError(
+                'Barcode name must be unique in barcode set!')
+
+    def __str__(self):
+        return '{} ({})'.format(self.name, self.sequence)
+
+    def __repr__(self):
+        tpl = 'BarcodeSetEntry({})'
+        values = (self.name, self.sequence)
+        return tpl.format(', '.join(map(repr, values)))
--- flowcells/tests/test_models.py
+++ flowcells/tests/test_models.py
@@ -10,6 +10,7 @@ from .. import models
 
 
 class TestSequencingMachine(TestCase):
+    """Tests for models.SequencingMachine"""
 
     def setUp(self):
         self.machine = self._make_machine()
@@ -49,3 +50,73 @@ class TestSequencingMachine(TestCase):
             "SequencingMachine('NS5001234', 'NextSeq#1', "
             "'In corner of lab 101', 'NextSeq', 1, 'A')")
         self.assertEqual(repr(self.machine), EXPECTED)
+
+
+class BarcodeSetMixin:
+    """Mixin for for _make_barcode_set()"""
+
+    def _make_barcode_set(self):
+        values = {
+            'name': 'Agilent SureSelect XT',
+            'short_name': 'SureSelect',
+        }
+        result = models.BarcodeSet(**values)
+        result.save()
+        return result
+
+
+class TestBarcodeSet(TestCase, BarcodeSetMixin):
+    """Tests for models.BarcodeSet"""
+
+    def setUp(self):
+        self.barcode_set = self._make_barcode_set()
+
+    def test_initialization(self):
+        EXPECTED = {
+            'id': self.barcode_set.pk,
+            'name': 'Agilent SureSelect XT',
+            'short_name': 'SureSelect',
+        }
+        self.assertEqual(model_to_dict(self.barcode_set), EXPECTED)
+
+    def test__str(self):
+        EXPECTED = """Agilent SureSelect XT (SureSelect)"""
+        self.assertEqual(str(self.barcode_set), EXPECTED)
+
+    def test__repr__(self):
+        EXPECTED = """BarcodeSet('Agilent SureSelect XT', 'SureSelect')"""
+        self.assertEqual(repr(self.barcode_set), EXPECTED)
+
+
+class TestBarcodeSetEntry(TestCase, BarcodeSetMixin):
+    """Tests for models.BarcodeSetEntry"""
+
+    def setUp(self):
+        self.barcode_set = self._make_barcode_set()
+        self.barcode = self._make_barcode_set_entry(self.barcode_set)
+
+    def _make_barcode_set_entry(self, barcode_set):
+        values = {
+            'name': 'AR01',
+            'sequence': 'ACGTGTTA',
+        }
+        result = models.BarcodeSetEntry(barcode_set=barcode_set, **values)
+        result.save()
+        return result
+
+    def test_initialization(self):
+        EXPECTED = {
+            'id': self.barcode.pk,
+            'barcode_set': self.barcode_set.pk,
+            'name': 'AR01',
+            'sequence': 'ACGTGTTA',
+        }
+        self.assertEqual(model_to_dict(self.barcode), EXPECTED)
+
+    def test__str(self):
+        EXPECTED = """AR01 (ACGTGTTA)"""
+        self.assertEqual(str(self.barcode), EXPECTED)
+
+    def test__repr__(self):
+        EXPECTED = """BarcodeSetEntry('AR01', 'ACGTGTTA')"""
+        self.assertEqual(repr(self.barcode), EXPECTED)
