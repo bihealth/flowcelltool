a0abe1761f0df35f6534e4b819df35800787746e
Manuel Holtgrewe <zyklenfrei@gmail.com>
2016-10-02 15:14:47 +0200

    Comprehensive testing for flow cell omdels

--- flowcells/models.py
+++ flowcells/models.py
@@ -1,30 +1,66 @@
+import re
+
 from django.db import models
 from django.core.exceptions import ValidationError
+from django.contrib.postgres.fields import ArrayField
+
+from flowcelltool.users.models import User
+from django.core.validators import MinValueValidator, MaxValueValidator, \
+    RegexValidator
 
 from markdown_deux.templatetags.markdown_deux_tags import markdown_allowed
 
 # SequencingMachine and related ----------------------------------------------
 
+#: Key value for machine model MiSeq
+MACHINE_MODEL_MISEQ = 'MiSeq'
+
+#: Key value for machine model MiniSeq
+MACHINE_MODEL_MINISEQ = 'MiniSeq'
+
+#: Key value for machine model NextSeq500
+MACHINE_MODEL_NEXTSEQ500 = 'NextSeq500'
+
+#: Key value for machine model HiSeq1000
+MACHINE_MODEL_HISEQ1000 = 'HiSeq1000'
+
+#: Key value for machine model HiSeq1500
+MACHINE_MODEL_HISEQ1500 = 'HiSeq1500'
+
+#: Key value for machine model HiSeq3000
+MACHINE_MODEL_HISEQ3000 = 'HiSeq3000'
+
+#: Key value for machine model HiSeq4000
+MACHINE_MODEL_HISEQ4000 = 'HiSeqHiSeq40001000'
+
+#: Key value for "other" machine models
+MACHINE_MODEL_OTHER = 'other'
+
 #: Choices for machine models
 MACHINE_MODELS = (
-    ('MiSeq', 'MiSeq'),
-    ('MiniSeq', 'MiniSeq'),
-    ('NextSeq500', 'NextSeq 500'),
-    ('HiSeq1000', 'HiSeq 1000'),
-    ('HiSeq1500', 'HiSeq 1500'),
-    ('HiSeq3000', 'HiSeq 3000'),
-    ('HiSeq4000', 'HiSeq 4000'),
-    ('other', 'Other'),  # be a bit more future proof
+    (MACHINE_MODEL_MISEQ, 'MiSeq'),
+    (MACHINE_MODEL_MINISEQ, 'MiniSeq'),
+    (MACHINE_MODEL_NEXTSEQ500, 'NextSeq 500'),
+    (MACHINE_MODEL_HISEQ1000, 'HiSeq 1000'),
+    (MACHINE_MODEL_HISEQ1500, 'HiSeq 1500'),
+    (MACHINE_MODEL_HISEQ3000, 'HiSeq 3000'),
+    (MACHINE_MODEL_HISEQ4000, 'HiSeq 4000'),
+    (MACHINE_MODEL_OTHER, 'Other'),  # be a bit more future proof
 )
 
+#: Key value for index workflow A
+INDEX_WORKFLOW_A = 'A'
+
+#: Key value for index workflow B
+INDEX_WORKFLOW_B = 'B'
 
 #: Choices for index workflows, determines whether the second index is read
 #: as reverse-complement or not for dual indexing.  Could be inferred from
 #: the machine type but not doing so as we would have to know all machine
 #: types at any time.
 INDEX_WORKFLOWS = (
-    ('A', 'MiSeq, HiSeq <=2500'),
-    ('B', 'MiniSeq, NextSeq, HiSeq >=3000'),
+    (INDEX_WORKFLOW_A, 'MiSeq, HiSeq <=2500'),
+    (INDEX_WORKFLOW_B, 'MiniSeq, NextSeq, HiSeq >=3000'),
 )
 
 
@@ -40,6 +76,7 @@ class SequencingMachine(models.Model):
     #: Vendor ID of the machine, reflected in file names and read names later
     #: on
     vendor_id = models.CharField(
+        unique=True, db_index=True,
         max_length=100,
         help_text='Vendor ID of the machine')
 
@@ -69,7 +106,7 @@ class SequencingMachine(models.Model):
     dual_index_workflow = models.CharField(
         max_length=10,
         choices=INDEX_WORKFLOWS,
-        default='A',
+        default=INDEX_WORKFLOW_A,
         help_text='Workflow to use for dual indexing')
 
     def __str__(self):
@@ -102,6 +139,13 @@ class BarcodeSet(models.Model):
     short_name = models.CharField(
         max_length=100, unique=True, db_index=True,
         help_text='Short, unique identifier of barcode adapter set')
+
+    #: Optional, short description of the barcode set, including copyright
+    #: notices etc.
+    description = models.TextField(
+        blank=True, null=True,
+        help_text=('Short description of the barcode set' +
+                   markdown_allowed()))
 
     def __str__(self):
         return '{} ({})'.format(self.name, self.short_name)
@@ -156,4 +200,292 @@ class BarcodeSetEntry(models.Model):
     def __repr__(self):
         tpl = 'BarcodeSetEntry({})'
         values = (self.name, self.sequence)
+        return tpl.format(', '.join(map(repr, values)))
+
+
+# FlowCell and related -------------------------------------------------------
+
+#: Flow cell status key for initial state
+FLOWCELL_STATUS_INITIAL = 'initial'
+
+#: Flow cell status key for sequencing complete
+FLOWCELL_STATUS_SEQ_COMPLETE = 'seq_complete'
+
+#: Flow cell status key for sequencing failed
+FLOWCELL_STATUS_SEQ_FAILED = 'seq_failed'
+
+#: Flow cell status key for demultiplexing complete
+FLOWCELL_STATUS_DEMUX_COMPLETE = 'demux_complete'
+
+#: Flow cell status key for demultiplexing delivered
+FLOWCELL_STATUS_DEMUX_DELIVERED = 'demux_delivered'
+
+#: Flow cell status choices
+FLOWCELL_STATUS_CHOICES = (
+    #: initial, known to the system but no sequencing or demultiplexing
+    #: happened yet
+    (FLOWCELL_STATUS_INITIAL, 'initial'),
+    #: sequencing complete, the files are completely written to the storage
+    #: volume
+    (FLOWCELL_STATUS_SEQ_COMPLETE, 'sequecing complete'),
+    #: sequencing failed, possibly there are files but the sequencing has
+    #: not succeeded
+    (FLOWCELL_STATUS_SEQ_FAILED, 'sequencing failed'),
+    #: demultiplexing has been completed
+    (FLOWCELL_STATUS_DEMUX_COMPLETE, 'demultiplexing complete'),
+    #: demultiplexed files have been moved to their final destination, either
+    #: in-house or to a customer/project partner
+    (FLOWCELL_STATUS_DEMUX_DELIVERED, 'demultiplexing results delivered'),
+)
+
+
+#: RTA version key for v1
+RTA_VERSION_V1 = 1
+
+#: RTA version key for v2
+RTA_VERSION_V2 = 2
+
+#: RTA version key for "other"
+RTA_VERSION_OTHER = 0
+
+#: RTA version used for a flow cell
+RTA_VERSION_CHOICES = (
+    #: RTA v1.x, old bcl2fastq required
+    (RTA_VERSION_V1, 'RTA v1'),
+    #: RTA v2.x, bcl2fast2 required
+    (RTA_VERSION_V2, 'RTA v2'),
+    #: other, for future-proofness
+    (RTA_VERSION_OTHER, 'other'),
+)
+
+
+#: Regular expression for flow cell names
+FLOW_CELL_NAME_RE = (
+    r'^(?P<date>\d{6,6})'
+    r'_(?P<machine_name>[^_]+)'
+    r'_(?P<run_no>\d+)'
+    r'_(?P<slot>\w)'
+    r'_(?P<vendor_id>[^_]+)'
+    r'(_(?P<label>.+))?$')
+
+
+class FlowCell(models.Model):
+    """Information stored for each flow cell"""
+
+    #: Timestamp for creation time
+    created_at = models.DateTimeField(auto_now_add=True)
+    #: Timestamp for last update
+    updated_at = models.DateTimeField(auto_now=True)
+
+    #: Owner of the flow cell.  Set to NULL when the user is deleted to
+    #: circumvent any possible data loss.  Users should be deactivated
+    #: instead of being deleted anyway
+    owner = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+
+    #: Full name of the flow cell, fulfilling the pattern
+    #: "YYMMDD_MACHINE_RUNNUMBER_SLOT_FLOWCELLID_LABEL"
+    name = models.CharField(
+        max_length=100,
+        validators=[
+            RegexValidator(FLOW_CELL_NAME_RE,
+                           message='Invalid flow cell name')],
+        help_text=('The full flow cell name, e.g., '
+                   '160303_ST-K12345_0815_A_BCDEFGHIXX_LABEL'))
+
+    #: The sequencer used for processing this flow cell
+    sequencing_machine = models.ForeignKey(
+        SequencingMachine, null=True, on_delete=models.SET_NULL)
+
+    #: Number of lanes on the flow cell
+    num_lanes = models.IntegerField(
+        default=8,
+        help_text='Number of lanes on flowcell 8 for HiSeq, 4 for NextSeq')
+
+    #: Status of the flow cell, as tracked with this app
+    status = models.CharField(
+        max_length=50, default='initial',
+        choices=FLOWCELL_STATUS_CHOICES,
+        help_text='Processing status of flow cell')
+
+    #: Name of the run operator
+    operator = models.CharField(max_length=100)
+
+    #: Whether or not the run was executed in a paired-end fashion
+    is_paired = models.BooleanField(
+        default=False, help_text='Check for paired reads')
+
+    #: Number of index reads used.
+    index_read_count = models.IntegerField(
+        validators=[MinValueValidator(0), MaxValueValidator(2)],
+        default=1, help_text='Number of index reads that were used (0..2)')
+
+    #: RTA version used, required for picking BCL to FASTQ and demultiplexing
+    #: software
+    rta_version = models.IntegerField(
+        default=RTA_VERSION_V2, choices=RTA_VERSION_CHOICES,
+        help_text='Major RTA version, implies bcl2fastq version')
+
+    #: Read length that was configured
+    read_length = models.IntegerField(default=151)
+
+    def save(self, *args, **kwargs):
+        self._validate_sequencer()
+        self._validate_num_lanes()
+        super().save(*args, **kwargs)
+
+    def _validate_sequencer(self):
+        """Check that the sequencer given in the name exists, then update
+        the machine attribute to the corresponding record
+        """
+        m = re.match(FLOW_CELL_NAME_RE, self.name)
+        if not m:
+            raise ValidationError(
+                'Invalid flow cell name, cannot infer machine name')
+        machine_name = m.group('machine_name')
+        qs = SequencingMachine.objects.filter(vendor_id=machine_name)
+        if not qs.exists():
+            raise ValidationError(
+                'Sequencing machine named {} not found'.format(machine_name))
+        else:
+            self.sequencing_machine = qs.all()[0]
+
+    def _validate_num_lanes(self):
+        """Check that the num_lanes value is compatible with any contained
+        Library
+        """
+        for library in self.libraries.all():
+            if any (l > self.num_lanes for l in library.lane_numbers):
+                raise ValidationError(
+                    'Library {} is on lane [{}] (> {})'.format(
+                        library.name, list(sorted(library.lane_numbers)),
+                        self.num_lanes))
+
+    def __str__(self):
+        return str(self.name)
+
+    def __repr__(self):
+        tpl = 'FlowCell({})'
+        values = (self.name, self.sequencing_machine,
+                  self.num_lanes, self.status, self.operator, self.is_paired,
+                  self.index_read_count, self.rta_version, self.read_length)
+        return tpl.format(', '.join(map(repr, values)))
+
+
+#: Reference used for identifying human samples
+REFERENCE_HUMAN = 'hg19'
+
+#: Reference used for identifying mouse samples
+REFERENCE_MOUSE = 'mm9'
+
+#: Reference used for identifying fly samples
+REFERENCE_FLY = 'dm6'
+
+#: Reference used for identifying fish samples
+REFERENCE_FISH = 'danRer6'
+
+#: Reference used for identifying rat samples
+REFERENCE_RAT = 'rn11'
+
+#: Reference used for identifying worm samples
+REFERENCE_WORM = 'ce11'
+
+#: Reference used for identifying yeast samples
+REFERENCE_YEAST = 'sacCer3'
+
+#: Reference used for identifying other samples
+REFERENCE_OTHER = '__other__'
+
+#: Reference sequence choices, to identify organisms
+REFERENCE_CHOICES = (
+    #: H. sapiens
+    (REFERENCE_HUMAN, 'human'),
+    #: M. musculus
+    (REFERENCE_MOUSE, 'mouse'),
+    #: D. melanogaster
+    (REFERENCE_FLY, 'fly'),
+    #: D. rerio
+    (REFERENCE_FISH, 'zebrafish'),
+    #: R. norvegicus
+    (REFERENCE_RAT, 'rat'),
+    #: C. elegans
+    (REFERENCE_WORM, 'worm'),
+    #: S. cerevisae
+    (REFERENCE_YEAST, 'yeast'),
+    #: other
+    (REFERENCE_OTHER, 'other'),
+)
+
+
+class Library(models.Model):
+    """The data stored for each library that is to be sequenced
+    """
+
+    #: Timestamp for creation time
+    created_at = models.DateTimeField(auto_now_add=True)
+    #: Timestamp for last update
+    updated_at = models.DateTimeField(auto_now=True)
+
+    #: The flow cell that this library has been sequenced on
+    flow_cell = models.ForeignKey(FlowCell, related_name='libraries',
+                                  on_delete=models.CASCADE)
+
+    #: The name of the library
+    name = models.CharField(max_length=100)
+
+    #: The organism to assume for this library, used for QC
+    reference = models.CharField(
+        max_length=100, default='hg19', choices=REFERENCE_CHOICES)
+
+    #: The barcode set used for this library
+    barcode_set = models.ForeignKey(BarcodeSet)
+
+    #: The barcode used for this library
+    barcode = models.ForeignKey(BarcodeSetEntry)
+
+    #: The lanes that the library was sequenced on on the flow cell
+    lane_numbers = ArrayField(
+        models.IntegerField(validators=[MinValueValidator(1)]))
+
+    def save(self, *args, **kwargs):
+        """ Override to check name/barcode being unique on the flow cell
+        lanes and lane numbers are compatible
+        """
+        self._validate_uniqueness()
+        self._validate_lane_nos()
+        return super().save(*args, **kwargs)
+
+    def _validate_lane_nos(self):
+        if any(l > self.flow_cell.num_lanes for l in self.lane_numbers):
+            raise ValidationError(
+                'Lane no {} > flow cell lane count {}'.format(
+                    list(sorted(self.lane_numbers)),
+                    self.flow_cell.num_lanes))
+
+    def _validate_uniqueness(self):
+        # Get all libraries sharing any lane on the same flow cell
+        libs_on_lanes = Library.objects.filter(
+            flow_cell=self.flow_cell,
+            lane_numbers__overlap=self.lane_numbers
+        ).exclude(
+            pk=self.pk
+        )
+        # Check that no libraries exist with the same
+        if libs_on_lanes.filter(name=self.name).exists():
+            raise ValidationError(
+                ('There are libraries sharing flow cell lane with the '
+                 'same name as {}'.format(self.name)))
+        # Check that no libraries exist with the same barcode
+        if libs_on_lanes.filter(barcode=self.barcode).exists():
+            raise ValidationError(
+                ('There are libraries sharing flow cell lane with the '
+                 'same barcode as {}: {}'.format(self.name, self.barcode)))
+
+    def __str__(self):
+        values = (self.name, self.barcode.name, self.barcode.sequence)
+        return '{} ({}:{})'.format(*map(str, values))
+
+    def __repr__(self):
+        tpl = 'Library({})'
+        values = (self.flow_cell.name, self.reference, self.barcode_set,
+                  self.barcode, self.lane_numbers)
         return tpl.format(', '.join(map(repr, values)))
--- flowcells/tests/test_models.py
+++ flowcells/tests/test_models.py
@@ -10,11 +10,8 @@ from django.core.exceptions import ValidationError
 from .. import models
 
 
-class TestSequencingMachine(TestCase):
-    """Tests for models.SequencingMachine"""
-
-    def setUp(self):
-        self.machine = self._make_machine()
+class SequencingMachineMixin:
+    """Helper mixin class to provide _make_machine"""
 
     def _make_machine(self):
         """Return SequencingMachine instance for testing"""
@@ -22,34 +19,43 @@ class TestSequencingMachine(TestCase):
             'vendor_id': 'NS5001234',
             'label': 'NextSeq#1',
             'description': 'In corner of lab 101',
-            'machine_model': 'NextSeq',
+            'machine_model': models.MACHINE_MODEL_NEXTSEQ500,
             'slot_count': 1,
-            'dual_index_workflow': 'A',
+            'dual_index_workflow': models.INDEX_WORKFLOW_A,
         }
-        return models.SequencingMachine(**values)
+        result = models.SequencingMachine(**values)
+        result.save()
+        return result
+
+
+class TestSequencingMachine(TestCase, SequencingMachineMixin):
+    """Tests for models.SequencingMachine"""
+
+    def setUp(self):
+        self.machine = self._make_machine()
 
     def test_initialization(self):
         EXPECTED = {
-            'id': None,
+            'id': self.machine.pk,
             'vendor_id': 'NS5001234',
             'label': 'NextSeq#1',
             'description': 'In corner of lab 101',
-            'machine_model': 'NextSeq',
+            'machine_model': models.MACHINE_MODEL_NEXTSEQ500,
             'slot_count': 1,
-            'dual_index_workflow': 'A',
+            'dual_index_workflow': models.INDEX_WORKFLOW_A,
         }
         self.assertEqual(model_to_dict(self.machine), EXPECTED)
 
     def test__str__(self):
         EXPECTED = (
             "SequencingMachine('NS5001234', 'NextSeq#1', "
-            "'In corner of lab 101', 'NextSeq', 1, 'A')")
+            "'In corner of lab 101', 'NextSeq500', 1, 'A')")
         self.assertEqual(str(self.machine), EXPECTED)
 
     def test__repr__(self):
         EXPECTED = (
             "SequencingMachine('NS5001234', 'NextSeq#1', "
-            "'In corner of lab 101', 'NextSeq', 1, 'A')")
+            "'In corner of lab 101', 'NextSeq500', 1, 'A')")
         self.assertEqual(repr(self.machine), EXPECTED)
 
 
@@ -75,6 +81,7 @@ class TestBarcodeSet(TestCase, BarcodeSetMixin):
     def test_initialization(self):
         EXPECTED = {
             'id': self.barcode_set.pk,
+            'description': None,
             'name': 'Agilent SureSelect XT',
             'short_name': 'SureSelect',
         }
@@ -89,22 +96,27 @@ class TestBarcodeSet(TestCase, BarcodeSetMixin):
         self.assertEqual(repr(self.barcode_set), EXPECTED)
 
 
-class TestBarcodeSetEntry(TestCase, BarcodeSetMixin):
-    """Tests for models.BarcodeSetEntry"""
-
-    def setUp(self):
-        self.barcode_set = self._make_barcode_set()
-        self.barcode = self._make_barcode_set_entry(self.barcode_set)
+class BarcodeSetEntryMixin:
+    """Mixin for for _make_barcode_set()"""
 
     def _make_barcode_set_entry(
             self, barcode_set, name='AR01', sequence='ACGTGTTA'):
         values = {
             'name': name,
             'sequence': sequence,
+            'barcode_set': barcode_set,
         }
-        result = models.BarcodeSetEntry(barcode_set=barcode_set, **values)
+        result = models.BarcodeSetEntry(**values)
         result.save()
         return result
+
+
+class TestBarcodeSetEntry(TestCase, BarcodeSetEntryMixin, BarcodeSetMixin):
+    """Tests for models.BarcodeSetEntry"""
+
+    def setUp(self):
+        self.barcode_set = self._make_barcode_set()
+        self.barcode = self._make_barcode_set_entry(self.barcode_set)
 
     def test_initialization(self):
         EXPECTED = {
@@ -115,7 +127,7 @@ class TestBarcodeSetEntry(TestCase, BarcodeSetMixin):
         }
         self.assertEqual(model_to_dict(self.barcode), EXPECTED)
 
-    def test__str(self):
+    def test__str__(self):
         EXPECTED = """AR01 (ACGTGTTA)"""
         self.assertEqual(str(self.barcode), EXPECTED)
 
@@ -132,3 +144,167 @@ class TestBarcodeSetEntry(TestCase, BarcodeSetMixin):
         """Check uniqueness of the sequence is enforced"""
         with self.assertRaises(ValidationError):
             self._make_barcode_set_entry(self.barcode_set, 'ARNN', 'ACGTGTTA')
+
+
+class FlowCellMixin:
+    """Helper mixin that provides _make_flow_cell()"""
+
+    def _make_flow_cell(
+            self, owner, name, num_lanes, status, operator, is_paired,
+            index_read_count, rta_version, read_length):
+        values = {
+            'owner': owner,
+            'name': name,
+            'num_lanes': num_lanes,
+            'status': status,
+            'operator': operator,
+            'is_paired': is_paired,
+            'index_read_count': index_read_count,
+            'rta_version': rta_version,
+            'read_length': read_length,
+        }
+        result = models.FlowCell(**values)
+        result.save()
+        return result
+
+
+class TestFlowCell(TestCase, SequencingMachineMixin, FlowCellMixin,
+                   BarcodeSetEntryMixin, BarcodeSetMixin):
+
+    def setUp(self):
+        self.user = self.make_user()
+        self.machine = self._make_machine()
+        self.barcode_set = self._make_barcode_set()
+        self.barcode = self._make_barcode_set_entry(self.barcode_set)
+        self.flow_cell_name = '160303_{}_0815_A_BCDEFGHIXX_LABEL'.format(
+            self.machine.vendor_id)
+        self.flow_cell = self._make_flow_cell(
+            self.user, self.flow_cell_name, 8,
+            models.FLOWCELL_STATUS_SEQ_COMPLETE, 'John Doe',
+            True, 1, models.RTA_VERSION_V2, 151)
+
+    def test_initialization(self):
+        EXPECTED = {
+            'id': self.flow_cell.pk,
+            'owner': self.user.pk,
+            'name': self.flow_cell_name,
+            'sequencing_machine': self.machine.pk,
+            'num_lanes': 8,
+            'status': models.FLOWCELL_STATUS_SEQ_COMPLETE,
+            'operator': 'John Doe',
+            'is_paired': True,
+            'index_read_count': 1,
+            'rta_version': models.RTA_VERSION_V2,
+            'read_length': 151,
+        }
+        self.assertEqual(model_to_dict(self.flow_cell), EXPECTED)
+
+    def test__str__(self):
+        EXPECTED = '160303_NS5001234_0815_A_BCDEFGHIXX_LABEL'
+        self.assertEqual(str(self.flow_cell), EXPECTED)
+
+    def test__repr__(self):
+        EXPECTED = (
+            r"""FlowCell('160303_NS5001234_0815_A_BCDEFGHIXX_LABEL', """
+            r"""SequencingMachine('NS5001234', 'NextSeq#1', 'In corner of """
+            r"""lab 101', 'NextSeq500', 1, 'A'), 8, 'seq_complete', """
+            r"""'John Doe', True, 1, 2, 151)""")
+        self.assertEqual(repr(self.flow_cell), EXPECTED)
+
+    def test_validate_sequencer(self):
+        self._make_flow_cell(
+            self.user, self.flow_cell_name, 8,
+            models.FLOWCELL_STATUS_SEQ_COMPLETE, 'John Doe',
+            True, 1, models.RTA_VERSION_V2, 151)
+
+
+class LibraryMixin:
+    """Helper mixin that provides _make_library()"""
+
+    def _make_library(
+            self, flow_cell, name, reference, barcode_set, barcode,
+            lane_numbers):
+        values = {
+            'flow_cell': flow_cell,
+            'name': name,
+            'reference': reference,
+            'barcode_set': barcode_set,
+            'barcode': barcode,
+            'lane_numbers': lane_numbers,
+        }
+        result = models.Library(**values)
+        result.save()
+        return result
+
+
+class Library(TestCase, LibraryMixin, SequencingMachineMixin, FlowCellMixin,
+              BarcodeSetEntryMixin, BarcodeSetMixin):
+
+    def setUp(self):
+        self.user = self.make_user()
+        self.machine = self._make_machine()
+        self.barcode_set = self._make_barcode_set()
+        self.barcode = self._make_barcode_set_entry(self.barcode_set)
+        self.barcode2 = self._make_barcode_set_entry(
+            self.barcode_set, 'AR02', 'CGATATA')
+        self.flow_cell_name = '160303_{}_0815_A_BCDEFGHIXX_LABEL'.format(
+            self.machine.vendor_id)
+        self.flow_cell = self._make_flow_cell(
+            self.user, self.flow_cell_name, 8,
+            models.FLOWCELL_STATUS_SEQ_COMPLETE, 'John Doe',
+            True, 1, models.RTA_VERSION_V2, 151)
+        self.library = self._make_library(
+            self.flow_cell, 'LIB_001', models.REFERENCE_HUMAN,
+            self.barcode_set, self.barcode, [1, 2])
+
+    def test_initialization(self):
+        EXPECTED = {
+            'id': self.flow_cell.pk,
+            'barcode': self.barcode.pk,
+            'barcode_set': self.barcode_set.pk,
+            'flow_cell': self.flow_cell.pk,
+            'lane_numbers': [1, 2],
+            'name': 'LIB_001',
+            'reference': models.REFERENCE_HUMAN
+        }
+        self.assertEqual(model_to_dict(self.library), EXPECTED)
+
+    def test__str__(self):
+        EXPECTED = 'LIB_001 (AR01:ACGTGTTA)'
+        self.assertEqual(str(self.library), EXPECTED)
+
+    def test__repr__(self):
+        EXPECTED = (
+            r"""Library('160303_NS5001234_0815_A_BCDEFGHIXX_LABEL', """
+            r"""'hg19', BarcodeSet('Agilent SureSelect XT', 'SureSelect'), """
+            r"""BarcodeSetEntry('AR01', 'ACGTGTTA'), [1, 2])""")
+        self.assertEqual(repr(self.library), EXPECTED)
+
+    def test_validate_uniqueness_violate_name(self):
+        with self.assertRaises(ValidationError):
+            self._make_library(
+                self.flow_cell, 'LIB_001', models.REFERENCE_HUMAN,
+                self.barcode_set, self.barcode2, [2])
+
+    def test_validate_uniqueness_violate_barcode(self):
+        with self.assertRaises(ValidationError):
+            self._make_library(
+                self.flow_cell, 'LIB_002', models.REFERENCE_HUMAN,
+                self.barcode_set, self.barcode, [2])
+
+    def test_validate_lane_nos(self):
+        with self.assertRaises(ValidationError):
+            self._make_library(
+                self.flow_cell, 'LIB_002', models.REFERENCE_HUMAN,
+                self.barcode_set, self.barcode, [9, 10])
+
+    def test_validate_flow_cell_lane_nos_(self):
+        """Check that the lane count is compatible with lane numbers in the
+        contained libraries
+        """
+        library2 = self._make_library(
+            self.flow_cell, 'LIB_001', models.REFERENCE_HUMAN,
+            self.barcode_set, self.barcode2, [8])
+        self.flow_cell.num_lanes = 4
+        with self.assertRaises(ValidationError):
+            self.flow_cell.save()
