badd7ea2454ee0098df665590fb16fa1dc4a2851
Manuel Holtgrewe <manuel.holtgrewe@bihealth.de>
2016-10-17 09:34:51 +0200

    Adding guest group

--- flowcelltool/flowcells/migrations/0002_create_groups.py
+++ flowcelltool/flowcells/migrations/0002_create_groups.py
@@ -6,6 +6,7 @@ from __future__ import unicode_literals
 from django.db import migrations
 
 
+GUEST = 'Guest'
 INSTRUMENT_OPERATOR = 'Instrument Operator'
 DEMUX_OPERATOR = 'Demultiplexing Operator'
 DEMUX_ADMIN = 'Demultiplexing Admin'
@@ -15,6 +16,8 @@ IMPORT_BOT = 'Import Bot'
 def make_groups(apps, schema_editor):
     """Create initial groups"""
     Group = apps.get_model('auth', 'Group')
+    # Guest (read-only)
+    guest, _ = Group.objects.get_or_create(name=GUEST)
     # Instrument operator
     inst_op, _ = Group.objects.get_or_create(name=INSTRUMENT_OPERATOR)
     # Demultiplexing operator
@@ -28,7 +31,7 @@ def make_groups(apps, schema_editor):
 def delete_groups(apps, schema_editor):
     """Remove initial groups again"""
     Group = apps.get_model('auth', 'Group')
-    for name in (INSTRUMENT_OPERATOR, DEMUX_OPERATOR, IMPORT_BOT):
+    for name in (GUEST, INSTRUMENT_OPERATOR, DEMUX_OPERATOR, IMPORT_BOT):
         Group.objects.filter(name=name).delete()
 
 
