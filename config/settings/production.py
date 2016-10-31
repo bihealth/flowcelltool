09edaf6b1776f9f0f3ab56cdc5ab15cb69e13d4b
Manuel Holtgrewe <manuel.holtgrewe@bihealth.de>
2016-10-31 16:48:45 +0100

    Removing email sending on errors (fixes #10)

--- config/settings/production.py
+++ config/settings/production.py
@@ -119,11 +119,11 @@ LOGGING = {
         },
     },
     'handlers': {
-        'mail_admins': {
-            'level': 'ERROR',
-            'filters': ['require_debug_false'],
-            'class': 'django.utils.log.AdminEmailHandler'
-        },
+        # 'mail_admins': {
+        #     'level': 'ERROR',
+        #     'filters': ['require_debug_false'],
+        #     'class': 'django.utils.log.AdminEmailHandler'
+        # },
         'console': {
             'level': 'DEBUG',
             'class': 'logging.StreamHandler',
