3920c608c7208fe04799ec376c148efa1d2126c6
Manuel Holtgrewe <manuel.holtgrewe@bihealth.de>
2016-12-21 09:59:26 +0100

    Inverting sort order of flow cells, latest now on top

--- flowcelltool/flowcells/views.py
+++ flowcelltool/flowcells/views.py
@@ -291,7 +291,9 @@ class FlowCellListView(
 
     permission_required = 'flowcells.list_flowcell'
 
-    queryset = models.FlowCell.objects.order_by('run_date')
+    #: Flow cells are sorted by run date (inferred from the name when being
+    #: created, latest come first)
+    queryset = models.FlowCell.objects.order_by('-run_date')
 
     #: Pagination with 50 items should work fine for us
     paginate_by = 50
